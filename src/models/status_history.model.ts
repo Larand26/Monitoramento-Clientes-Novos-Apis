89aa499c0a4391d45cdebd9e2949f45b8848ea9a
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-08-04 14:38:26 -0300

    feat: cria o model status_history.model.ts

---
+++ src/models/status_history.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const statusHistorySchema = new mongoose.Schema({
+  client_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Client",
+    required: true,
+  },
+  previous_status: {
+    type: String,
+    required: true,
+    enum: ["IN_CRM", "LOST", "SUCCESS"],
+  },
+  new_status: {
+    type: String,
+    required: true,
+    enum: ["IN_CRM", "LOST", "SUCCESS"],
+  },
+  changed_at: { type: Date, required: true },
+  order_id: { type: String, required: true },
+});
+
+const StatusHistoryModel = mongoose.model(
+  "StatusHistory",
+  statusHistorySchema,
+  "status_history",
+);
+export default StatusHistoryModel;
