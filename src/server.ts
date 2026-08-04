aae78875e3260ed59c3c0cfb1c223abedc74a1db
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-08-04 09:29:54 -0300

    style: implementa logger

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import appConfig from "./config/app.config.js";
 
 import App from "./App.js";
 
+import { logger } from "./utils/logger.js";
+
 const app = new App().server;
 
 async function startServer() {
   const port = appConfig.api.port;
   app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    logger.success(`Server is running on port ${port}`);
   });
 }
 
